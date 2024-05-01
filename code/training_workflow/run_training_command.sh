4c00ccb5125c3d56f805b0b196e8d5f9a817393b
Ghaida Al-Atoum <ghaida.alatoum2@gmail.com>
2024-05-01 18:24:32 -0500

    [CLEAN UP]

--- code/training_workflow/run_training_command.sh
+++ code/training_workflow/run_training_command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python3 ./train_small.py \
-    --model_config_path /notebooks/code/training_workflow/model_8_layers_3_kernel_size_no_flat.yaml \
+    --model_config_path /notebooks/code/training_workflow/layer_configs/8_layers/with_flat/kernel_size_3.yaml \
     --logging_output_dir /notebooks/code/training_workflow/outputs
